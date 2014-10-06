f47e5273ef59062bd3eefa098e0cd4a961f805ef
Lilas Dinh <lvkdinh@gmail.com>
2014-10-05 23:39:49 -0400

    Created answer key to Part 1

--- Midterms/Exam1/Fall2013/pt1q2/Animal.java
+++ Midterms/Exam1/Fall2013/pt1q2/Animal.java
@@ -1,5 +1,8 @@
+// No import to ArrayList
+import java.util.ArrayList;
 public interface Animal extends Comparable{
-	public eat();
+	//	public eat(); No method type. Picked void for Lion.java
+	public void eat();
 	public void sleep();
 	public int drink(ArrayList list);
 }
--- Midterms/Exam1/Fall2013/pt1q2/Lion.java
+++ Midterms/Exam1/Fall2013/pt1q2/Lion.java
@@ -1,29 +1,42 @@
 import java.util.ArrayList;
+// There's no implementation of the Comparable class
 public class Lion implements Animal{
 	public static int weight = 8;
 	public int size = 0;
 	private String name = "none";
 	private static int count;
 
-	private void eat(){
+	// private void eat(){ <-- Change visibility
+	public void eat(){
 		System.out.println("sleeping");
 	}
 
 	public void sleep(){
 		double hours = 12/size;
-		int f = hours;
+		// int f = hours; <-- Improper casting
+		int f = (int) hours;
 	}
 
 	public int drink(ArrayList list){
 		return weight;
 	}
 
-	private static void run(){
+	// private static void run(){ <-- Change static to non-static
+	public void run(){
 		count += size;
 	}
 
 	public static int stop(){
 		int v = 5;
 		count = count - v;
+		// Forgot a return statement
+		return count;
+	}
+
+	// As required by Comparable
+	public int compareTo(Object o){
+		return 1; // <-- DO NOT DO THIS IN REAL LIFE
+		// DO THIS PROPERLY IN REAL LIFE.
+		// http://docs.oracle.com/javase/7/docs/api/java/lang/Comparable.html
 	}
 }
