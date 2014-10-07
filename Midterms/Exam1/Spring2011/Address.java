ce2067e58626fab8fbb681577ff97e0d73aa7937
Lilas Dinh <lvkdinh@gmail.com>
2014-10-07 01:31:41 -0400

    Finished study guide for Spring

---
+++ Midterms/Exam1/Spring2011/Address.java
@@ -0,0 +1,12 @@
+public class Address{
+	String addr;
+
+	public Address(String addr){
+		this.addr = addr;
+	}
+
+	@Override
+	public String toString(){
+		return this.addr;
+	}
+}
--- Midterms/Exam1/Spring2011/Exam1.java
+++ Midterms/Exam1/Spring2011/Exam1.java
@@ -1,12 +1,14 @@
 public class Exam1{
 	public int intVal;
-	private Address adress;
+	private Address address;
 
 	public static void main(String[] args){
 		Exam1 exam = new Exam1(5, "Cherry Lane");
 		Exam1 exam2 = update(exam, exam.getIntVal(), exam.getAddress());
 		System.out.println(exam);
 		System.out.println(exam2);
+		int x = 0;
+		System.out.println(7 / x);
 	}
 
 	public Exam1(int intValIn, String addressIn){
@@ -27,13 +29,21 @@ public class Exam1{
 		//prints out the entire String that was
 		//passed to the Address constructor
 		System.out.println("Address: " + address);
+		return "intVal: " + intVal + "\nAddress: " + address+"\n";
 	}
 
-	public static Exam1 update(Exam ex, int intValArg, Address addressArg){
+	public static Exam1 update(Exam1 ex, int intValArg, Address addressArg){
 		intValArg = 3;
 		addressArg = new Address("Pine Tree Lane");
-		ex = new Exam(intValArg, addressArg);
+		ex = new Exam1(intValArg, addressArg);
 		return ex;
 	}
 	// Assume standard getters and setters as seen in class
+	public Address getAddress(){
+		return this.address;
+	}
+
+	public int getIntVal(){
+		return this.intVal;
+	}
 }
